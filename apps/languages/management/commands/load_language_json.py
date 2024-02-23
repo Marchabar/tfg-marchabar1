0605eb7e990b61c5ef8689150385239c81275618
Marchabar <marchabar1@alum.us.es>
2024-02-23 12:32:52 +0100

    feat: language populate and unidecode

--- apps/languages/management/commands/load_language_json.py
+++ apps/languages/management/commands/load_language_json.py
@@ -1,7 +1,12 @@
+import glob
 import json
 
 from django.core.management.base import BaseCommand
-from languages.models import Language
+from unidecode import unidecode
+
+from apps.videos.models import Video
+
+from ...models import Language
 
 
 class Command(BaseCommand):
@@ -9,21 +14,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for filename in glob.glob("answers/*.json"):
-            with open(filename, "r") as file:
+            with open(filename, "r", encoding="utf-8") as file:
                 data = json.load(file)
                 valid_choices = [
                     choice[0]
                     for choice in Language._meta.get_field("language_type").choices
                 ]
-                for answer in data:
-                    # check if for each of the lenguaje in the json, there is a language choice
-                    for lenguaje in answer["lenguaje"]:
-                        if lenguaje not in valid_choices:
-                            break
-                        else:
-                            # get the url in the json and get the id of the video which has already been created containing this url
-                            video_id = Video.objects.get(url=answer["url"]).id
-                            # create a new language object
-                            Language.objects.create(
-                                language_type=lenguaje, video=video_id
-                            )
+                # check if for each of the lenguaje in the json, there is a language choice
+                for lenguaje in data.get("lenguaje"):
+                    lenguaje = unidecode(lenguaje.lower()).split()[-1]
+                    if lenguaje not in valid_choices:
+                        break
+                    elif Language.objects.filter(
+                        language_type=lenguaje,
+                        video=Video.objects.get(url=data.get("url")),
+                    ).exists():
+                        break
+                    else:
+                        video = Video.objects.get(url=data.get("url"))
+                        Language.objects.create(language_type=lenguaje, video=video)
