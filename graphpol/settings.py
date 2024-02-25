3adb989be5bdb6087a649c29bbdaaae6338b5f16
Marchabar <marchabar1@alum.us.es>
2024-02-25 14:52:01 +0100

    fix: youtube api key

--- graphpol/settings.py
+++ graphpol/settings.py
@@ -29,8 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-YOUTUBE_API_KEY = env("YOUTUBE_API_KEY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
