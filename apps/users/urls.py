ba2a33e2afe012f2aa81b01baadd474bd673b317
Marchabar <marchabar1@alum.us.es>
2024-03-04 13:20:41 +0100

    feat: add login wip register

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,4 +5,6 @@ from apps.users import views
 
 urlpatterns = [
     path("profile", views.index, name="my-profile"),
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.LogoutView.as_view(), name="logout"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.views import View
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "profile.html")
+
+
+class LoginView(View):
+    def get(self, request):
+        # Render the login form
+        return render(request, "login.html")
+
+    def post(self, request):
+        # Get username and password from the POST data
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        # Authenticate user
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            # User is authenticated, log in the user
+            login(request, user)
+            # Redirect to a success page or dashboard
+            return redirect(
+                "/"
+            )  # Replace 'dashboard' with the URL name of your dashboard page
+        else:
+            # Authentication failed, render login form with error message
+            return redirect("/")
+
+
+class LogoutView(View):
+    def post(self, request):
+        logout(request)
+        return redirect("/")
