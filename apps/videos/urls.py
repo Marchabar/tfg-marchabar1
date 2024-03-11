7a6466619214488bbf1d8f63bc339a87ba34e53f
Marchabar <marchabar1@alum.us.es>
2024-03-11 14:17:38 +0100

    feat: add analysis page

--- apps/videos/urls.py
+++ apps/videos/urls.py
@@ -4,5 +4,6 @@ from django.urls.conf import include
 from apps.videos import views
 
 urlpatterns = [
-    path("analysis", views.analizar_video_usuario, name="analysis"),
+    path("analysis", views.analyze_video_user, name="analysis"),
+    path("videos/<str:topic_type>", views.get_videos_by_topic, name="topic"),
 ]
--- apps/videos/views.py
+++ apps/videos/views.py
@@ -7,7 +7,7 @@ from apps.topics.models import Topic
 # Create your views here.
 
 
-def analizar_video_usuario(request):
+def analyze_video_user(request):
     if not request.user.is_authenticated:
         return redirect("/?login_required=true")
     # I need to get the 8 topics that appear in more videos taking into account a Topic has a video, a percentage and a topic_type
@@ -19,3 +19,17 @@ def analizar_video_usuario(request):
     top_topics = list(top_topics)
 
     return render(request, "analysis.html", {"top_topics": top_topics})
+
+
+def get_videos_by_topic(request, topic_type):
+    if not request.user.is_authenticated:
+        return redirect("/?login_required=true")
+
+    topics = Topic.objects.filter(topic_type=topic_type)
+    videos = []
+    for topic in topics:
+        videos.append(topic.video)
+
+    print(videos)
+
+    return render(request, "videos.html", {"videos": videos, "topic_type": topic_type})
